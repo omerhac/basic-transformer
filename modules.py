9dbce21d6bf6d2c86cffe4a19e63d8bad40a1f69
omerhac <omerhac94@gmail.com>
2020-10-15 00:26:07 +0300

    added EncoderLayer

--- modules.py
+++ modules.py
@@ -14,8 +14,8 @@ def scaled_dot_product_attention(q, k, v):
     return attention_values
 
 
-class MultyHeadAttention:
-    """Multy Head Attention layer as in MHA(Q,K,V) = concat(attention_head1, attention_head2, .., attention_headn)Wc,
+class MultiHeadAttention:
+    """Multi Head Attention layer as in MHA(Q,K,V) = concat(attention_head1, attention_head2, .., attention_headn)Wc,
     where attention_headi = attention(QWq, KWk, VWv). Wc, Wq, Wk and Wv are projections to d_c, d_q, d_k and d_v.
     """
 
@@ -74,11 +74,55 @@ class MultyHeadAttention:
 
         # concatenate and project
         concat_attention = tf.transpose(attention, perm=[0, 2, 1, 3])  # reorder to [batch_size, seq_length, num_heads, depth]
-        concat_attention = tf.reshape(attention, [batch_size, -1, self._d_model])  # concat to [batch_size, seq_length, d_model]
+        concat_attention = tf.reshape(concat_attention, [batch_size, -1, self._d_model])  # concat to [batch_size, seq_length, d_model]
 
         output = self._wc(concat_attention)
 
         return output
+
+
+class FeedForwardLayer:
+    """Two dense layers with relu on between"""
+    def __init__(self, d_model, dff):
+        self._f1 = tf.keras.layers.Dense(dff, activation='relu')
+        self._f2 = tf.keras.layers.Dense(d_model, activation='linear')
+        self._layer = tf.keras.Sequential([
+            self._f1,
+            self._f2
+        ])
+
+    def __call__(self, X):
+        return self._layer(X)
+
+
+class EncoderLayer:
+    """One encoder layer. Includes multi head attention and feed forwards sublayers. each sublayer output is:
+    output = LayerNorm(dropout(sublayer(sublayer_input)) + sublayer_input)"""
+    def __init__(self, d_model, attention_heads, d_feed_forward, dropout_rate):
+        self._mha = MultiHeadAttention(attention_heads, d_model)
+        self._ff = FeedForwardLayer(d_model, d_feed_forward)
+        self._d_model = d_model
+        self._mha_dropout = tf.keras.layers.Dropout(dropout_rate)  # multihead attention dropout
+        self._ffl_dropout = tf.keras.layers.Dropout(dropout_rate)  # feed forward dropout
+        self._mha_norm = tf.keras.layers.LayerNormalization()  # multihead attention LayerNorm
+        self._ffl_norm = tf.keras.layers.LayerNormalization()  # feed forward LayerNorm
+
+    def __call__(self, x):
+        # multihead attention
+        mha_output = self._mha(x, x, x)
+        mha_output = self._mha_dropout(mha_output)
+
+        # add mha to input and norm
+        s1 = self._mha_norm(x + mha_output)  # sublayer 1 output
+
+        # apply feed forward layer
+        ffl_output = self._ff(s1)
+        ffl_output = self._ffl_dropout(ffl_output)
+
+        # add ffl to s1 and norm
+        s2 = self._ffl_norm(s1 + ffl_output)
+
+        return s2
 
 
 if __name__ == '__main__':
@@ -98,6 +142,10 @@ if __name__ == '__main__':
     a = scaled_dot_product_attention(temp_q, temp_k, temp_v)
     print(a)
     x = tf.random.uniform([128, 10, 8])
-    mha = MultyHeadAttention(4, 8)
-    mha(x, x, x)
+    mha = MultiHeadAttention(4, 8)
+    print(mha(x, x, x).shape)
+    ffl = FeedForwardLayer(8, 200)
+    print(ffl(x).shape)
+    enc = EncoderLayer(8, 4, 100, 0.1)
+    print(enc(x).shape)
 
